7b0b4f47e57e585e3599829258dac913020a6ff2
FazioNico <contact@nicolasfazio.ch>
2018-06-14 10:38:43 +0200

    create detail comp. + module

---
+++ src/app/features/detail/detail.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DetailRoutingModule } from './detail-routing.module';
+import { DetailPageComponent } from './containers/detail-page/detail-page.component';
+import { IonicModule } from '@ionic/angular';
+import { SharedModule } from '../../shared/shared.module';
+
+const CONTAINERS = [
+  DetailPageComponent
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    IonicModule,
+    SharedModule,
+    DetailRoutingModule
+  ],
+  declarations: [
+    ...CONTAINERS
+  ]
+})
+export class DetailModule { }
