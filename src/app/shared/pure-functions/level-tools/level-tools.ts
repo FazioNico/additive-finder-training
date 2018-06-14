c53635cff6eba17319a3a50e64dbb0beda54e017
FazioNico <contact@nicolasfazio.ch>
2018-06-14 12:40:59 +0200

    create tools pure function

---
+++ src/app/shared/pure-functions/level-tools/level-tools.ts
@@ -0,0 +1,38 @@
+
+const getColor = (levelColor: string) => {
+  let classColor = '#4caf50';
+  switch (levelColor) {
+    case '0':
+      classColor = '#4caf50'; // green lighten
+      break;
+    case '1':
+      classColor = '#ffa726'; // orange lighten
+      break;
+    case '2':
+      classColor = '#ff7043'; // deep orange lighten
+      break;
+    case '3':
+      classColor = '#f44336'; // red lighten
+      break;
+  }
+  return classColor;
+};
+
+const getTXT = (levelColor: string) => {
+  let txt = 'Acceptable';
+  switch (levelColor) {
+    case '0':
+      txt = 'Acceptable';
+      break;
+    case '1':
+      txt = 'rapports contradictoires';
+      break;
+    case '2':
+      txt = 'à risque';
+      break;
+    case '3':
+      txt = 'à éviter';
+      break;
+  }
+  return txt.toUpperCase();
+};
